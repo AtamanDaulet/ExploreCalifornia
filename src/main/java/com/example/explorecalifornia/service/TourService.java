c0f981b9cf45605a976619d9947d43be78314f8e
Daulet Atambayev <atamandaulet@gmail.com>
2022-06-20 15:02:20 -0500

    california tours project started pack

---
+++ src/main/java/com/example/explorecalifornia/service/TourService.java
@@ -0,0 +1,62 @@
+package com.example.explorecalifornia.service;
+
+import com.example.explorecalifornia.domain.Difficulty;
+import com.example.explorecalifornia.domain.Region;
+import com.example.explorecalifornia.domain.Tour;
+import com.example.explorecalifornia.domain.TourPackage;
+import com.example.explorecalifornia.repo.TourPackageRepository;
+import com.example.explorecalifornia.repo.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Tour  Service
+ */
+
+@Service
+public class TourService {
+    private TourRepository tourRepository;
+    private TourPackageRepository tourPackageRepository;
+
+    @Autowired
+    public TourService(TourRepository tourRepository, TourPackageRepository tourPackageRepository) {
+        this.tourRepository = tourRepository;
+        this.tourPackageRepository = tourPackageRepository;
+    }
+
+    /**
+     * Create a new Tour Object and persist it to the Database.
+     *
+     * @param title
+     * @param description
+     * @param blurb
+     * @param price
+     * @param duration
+     * @param bullets
+     * @param keywords
+     * @param tourPackageName
+     * @param difficulty
+     * @param region
+     * @return Tour Entity
+     */
+    public Tour createTour(String title, String description, String blurb, Integer price,
+                           String duration, String bullets,
+                           String keywords, String tourPackageName, Difficulty difficulty, Region region ) {
+        TourPackage tourPackage = tourPackageRepository.findByName(tourPackageName).orElseThrow(() ->
+                new RuntimeException("Tour package does not exist: " + tourPackageName));
+
+        return tourRepository.save(new Tour(title, description,blurb, price, duration,
+                bullets, keywords, tourPackage, difficulty, region));
+    }
+
+    /**
+     * Calculate the number of Tours in the Database.
+     *
+     * @return the total.
+     */
+    public long total() {
+        return tourRepository.count();
+    }
+
+}
+
